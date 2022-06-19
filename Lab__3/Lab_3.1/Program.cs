dd5b1630ee43cec8ec3f3ebb028dbad5ee4ebf5f
Mirzoieva <107805547+Mirzoieva@users.noreply.github.com>
2022-06-19 21:41:31 +0300

    Add files via upload

---
+++ Lab__3/Lab_3.1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOfInt = new List<int>();
+            
+            Random random = new Random();
+            listOfInt.Add(random.Next(-100, 100));
+            listOfInt.Add(random.Next(-100, 100));
+            listOfInt.Add(random.Next(-100, 100));
+            listOfInt.Add(random.Next(-100, 100));
+            listOfInt.Add(random.Next(-100, 100));
+            listOfInt.Add(random.Next(-100, 100));
+
+            print(listOfInt);
+
+            Console.WriteLine("Sort...");
+            sort(listOfInt);
+
+            Console.WriteLine("Sorted list:");
+            print(listOfInt);
+
+            Console.ReadLine();
+        }
+
+        static void sort(List<int> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (list[j - 1] < list[j] && !((list[j - 1] > 0 && list[j] > 0) || ((list[j - 1] < 0 && list[j] < 0))))
+                    {
+                        list[j - 1]=list[j-1]+ list[j];
+                        list[j] = list[j] - list[j - 1];
+                        list[j] = -list[j];
+                        list[j-1] = list[j-1]- list[j];
+                    }
+                }
+            }
+        }
+
+        static void print(List<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+        }
+    }
+}
---
+++ Lab__3/Lab_3.2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Lab_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Amount;
+            int Number;
+
+            Console.WriteLine("Введiть кiлькiсть елементiв словника:");
+            Amount = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введiть ключ, значення пiсля якого будуть виведенi: ");
+            Number = Convert.ToInt32(Console.ReadLine());
+
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+            Random random = new Random();
+
+            for (int i = 0; i < Amount; i++)
+            {
+                dictionary.Add(i,random.Next(-100, 100));
+            }
+            for (int i = 0; i < Amount; i++)
+            {
+                Console.Write(dictionary[i] + " ");
+            }
+            Console.WriteLine();
+        
+            Console.WriteLine("Обранi елементи:");
+
+            Dictionary<int, int> choosendictionary =sort(dictionary, Number);
+
+            if (choosendictionary != null)
+            {
+                string str = JsonConvert.SerializeObject(choosendictionary);
+                File.WriteAllText("Dictionary.json", str);
+
+                Console.WriteLine("Iнформацiя з JSON файлу");
+                Console.WriteLine(File.ReadAllText("Dictionary.json"));
+            }
+
+            Console.ReadLine();
+        }
+        static Dictionary<int,int> sort(Dictionary<int, int> keyValues, int Number)
+        {
+            Dictionary<int, int> choosendictionary = new Dictionary<int, int>();
+            if (Number < keyValues.Count && Number >= 0)
+            {
+                foreach (KeyValuePair<int, int> el in keyValues)
+                {
+                    if (el.Key >= Number)
+                    {
+                        choosendictionary.Add(el.Key, el.Value);
+                        Console.WriteLine("Key: " + el.Key + " Value: " + el.Value);
+                    }
+
+                }
+                return choosendictionary;
+            }
+            else
+            {
+                Console.WriteLine("Такого ключа не iснує");
+                return null;
+            }
+        }
+
+}
+}
+
